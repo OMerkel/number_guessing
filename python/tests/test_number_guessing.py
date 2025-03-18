fa379b87f7b7fc66dc24aff57c8a47bc57414409
Oliver Merkel <Merkel.Oliver@web.de>
2025-03-18 10:00:23 +0100

    Update test_number_guessing.py

--- python/tests/test_number_guessing.py
+++ python/tests/test_number_guessing.py
@@ -6,7 +6,7 @@ from number_guessing import main
 
 
 def test_number_guessing_correct(monkeypatch):
-    """ Test for correct number guessing
+    """Test for correct number guessing
 
     GIVEN a number to guess and the correct number
     WHEN the user guesses the correct number
@@ -17,12 +17,12 @@ def test_number_guessing_correct(monkeypatch):
     expected_value = True
     attempts = 6
     inputs = iter([expected_guess])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     assert guess_number(expected_guess, attempts) is expected_value
 
 
 def test_number_guessing_too_low(monkeypatch):
-    """ Test for number guessing too low
+    """Test for number guessing too low
 
     GIVEN a number to guess
     AND a set of guesses finally listing the correct number
@@ -37,12 +37,12 @@ def test_number_guessing_too_low(monkeypatch):
     lower_guess = 3
     attempts = 6
     inputs = iter([lower_guess, expected_guess])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     assert guess_number(expected_guess, attempts) is expected_value
 
 
 def test_number_guessing_too_high(monkeypatch):
-    """ Test for number guessing too high
+    """Test for number guessing too high
 
     GIVEN a number to guess
     AND a set of guesses finally listing the correct number
@@ -57,12 +57,12 @@ def test_number_guessing_too_high(monkeypatch):
     higher_guess = 7
     attempts = 6
     inputs = iter([higher_guess, expected_guess])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     assert guess_number(expected_guess, attempts) is expected_value
 
 
 def test_number_guessing_run_out_of_attempts(monkeypatch):
-    """ Test for running out of attempts
+    """Test for running out of attempts
 
     GIVEN a number to guess
     AND a set of guesses that are all incorrect
@@ -73,17 +73,17 @@ def test_number_guessing_run_out_of_attempts(monkeypatch):
     expected_value = False
     attempts = 6
     inputs = iter([1, 2, 3, 4, 5, 6])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     assert guess_number(expected_guess, attempts) is expected_value
 
 
 def test_main(monkeypatch):
-    """ Test for main function if guess is performed without attempts left
+    """Test for main function if guess is performed without attempts left
 
     GIVEN the situation that the user has no attempts left
     WHEN the main function is called
     THEN the function should return False
     """
     inputs = iter([5])
-    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     assert main(attempts_left=0) is False
